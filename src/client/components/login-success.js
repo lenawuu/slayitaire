552c3059e8cf896d21c90673670484b82f2ecdbf
lenawuu <lenawu24@gmail.com>
2024-12-03 20:52:07 -0600

    fix iurl

--- src/client/components/login-success.js
+++ src/client/components/login-success.js
@@ -15,7 +15,7 @@ export const LoginSuccess = ({ logIn }) => {
       //   localStorage.getItem("accessToken") === null
       // ) {
       async function getAccessToken() {
-        await fetch(`http://slayitaire.click/v1/getAccessToken?code=${code}`)
+        await fetch(`/v1/getAccessToken?code=${code}`)
           .then((response) => {
             return response.json();
           })
